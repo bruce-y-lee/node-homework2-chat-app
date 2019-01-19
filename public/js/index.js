626547d55a544be23a003e1bfe8b801e0f659c57
yg lee <yglee@ygs-MacBook-Air.local>
2019-01-18 16:59:35 -0800

    Scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,25 @@
 var socket = io();
 
+function scrollToBottom() {
+  //Selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+
+  //heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+    // console.log('Shoul scroll');
+    messages.scrollTop(scrollHeight);
+  }
+
+}
+
+
 socket.on('connect', function() {
     console.log('Connected to Server');
 
@@ -22,6 +42,7 @@ socket.on('newMessage', function(message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
 
 //using jQuery
   // var li = jQuery('<li></li>');
@@ -44,6 +65,7 @@ socket.on('newLocationMessage', function(message){
   })
 
   jQuery('#messages').append(html);
+  scrollToBottom();
   // var li = jQuery('<li></li>');
   // var a = jQuery('<a target = "_blank">My current location</a>');
   // // var a = jQuery('<a>My current location</a>');
